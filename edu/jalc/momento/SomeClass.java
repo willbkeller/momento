73bb574e839a859981397f46a5df8acb21ef8ba7
William Keller <williamkeller@Williams-MacBook-Pro.local>
2017-04-27 08:06:22 -0500

    update SomeClass

--- edu/jalc/momento/SomeClass.java
+++ edu/jalc/momento/SomeClass.java
@@ -3,9 +3,11 @@ package edu.jalc.momento;
 public class SomeClass{
   public static void main(String[] args) {
     Originator o = new Originator();
-    o.setState("hello");
+    o.setState("Hello");
     o.save();
     System.out.println(o.getStateFromCaretaker(0));
-
+    o.setState("Goodbye");
+    o.save();
+    System.out.println(o.getStateFromCaretaker(1));
   }
 }
